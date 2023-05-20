af4153ad4196e8c5faaf5babd8a81a7478f935ce
LUGGGI(Lukas Beck) <st166506@stud.uni-stuttgart.de>
2023-05-20 20:10:57 +0200

    added run_to_counter_value and comments

--- conveyor.py
+++ conveyor.py
@@ -1,8 +1,8 @@
 '''
-This module controls a conveyor, it inherits from machine
+This module controls a Conveyor, it inherits from Machine
 
 Author: Lukas Beck
-Date: 01.05.2023
+Date: 20.05.2023
 '''
 import threading
 from enum import Enum
@@ -14,25 +14,43 @@ from actuator import Actuator
 
 
 class State(Enum):
-    WAIT = 0
-    START = 1     
+    RUN = 1     
     END = 100
     ERROR = 999
 
 
 class Conveyor(Machine):
-    '''Controls a conveyor'''
+    '''Controls a conveyor
+    
+    run_to_stop_sensor(): Runs the Conveyor until the product has reached the stop sensor
+    run_to_counter_value(): Runs the Conveyor until the trigger_value of encoder is reached
+    '''
 
     def __init__(self, revpi, name: str):
+        '''Initializes the Sensor
+        
+        :revpi: RevPiModIO Object to control the motors and sensors
+        :name: Exact name of the machine in PiCtory (everything bevor first '_')
+        '''
         super().__init__(revpi, name)
         self.state = None
         log.debug("Created Conveyor: " + self.name)
 
+
     def __del__(self):
         log.debug("Destroyed Conveyor: " + self.name)
 
+
     def run_to_stop_sensor(self, direction: str, stop_sensor: str, start_sensor: str=None, stop_delay_in_ms=0, timeout_in_s=10, as_thread=False):
-        '''Runs the conveyor until the product has reached the stop sensor'''
+        '''Runs the Conveyor until the product has reached the stop sensor.
+        
+        :direction: Conveyor direction, (last part of whole name)
+        :stop_sensor: Stops Conveyor if detection occurs at this Sensor
+        :start_sensor: Waits with starting until detection occurs at Sensor
+        :stop_delay_in_ms: Runs for given ms after detection of stop_sensor
+        :timeout_in_s: Time after which an exception is raised
+        :as_thread: Runs the function as a thread
+        '''
         # call this function again as a thread
         if as_thread == True:
             self.thread = threading.Thread(target=self.run_to_stop_sensor, args=(direction, stop_sensor, stop_delay_in_ms, timeout_in_s), name=self.name)
@@ -41,9 +59,9 @@ class Conveyor(Machine):
         
         if start_sensor != None:
             # wait for start sensor to detect product
-            Sensor(self.revpi, start_sensor).wait_for_detect()
+            Sensor(self.revpi, start_sensor).wait_for_detect(timeout_in_s=timeout_in_s)
         
-        self.state = self.switch_state(State.START)
+        self.state = self.switch_state(State.RUN)
         try:
             motor = Actuator(self.revpi, self.name)
             motor.run_to_sensor(direction, stop_sensor, stop_delay_in_ms, timeout_in_s)
@@ -55,24 +73,32 @@ class Conveyor(Machine):
             self.state = self.switch_state(State.END)
             self.ready_for_transport = True
 
-    # TODO: Remove if not needed
-    # def run_for_time(self, direction: str, check_sensor: str, run_for_in_s=5, as_thread=False):
-    #     '''Runs the conveyor for given amount of seconds, checks for product with check sensor'''
-    #     # call this function again as a thread
-    #     if as_thread == True:
-    #         self.thread = threading.Thread(target=self.run_for_time, args=(direction, check_sensor, run_for_in_s), name=self.name)
-    #         self.thread.start()
-    #         return
 
-    #     self.state = self.switch_state(State.START)
-    #     try:
-    #         motor = Motor(self.revpi, self.name)
-    #         motor.run_for_time(direction, check_sensor, run_for_in_s)
-    #     except Exception as error:
-    #         log.exception(error)
-    #         self.state = self.switch_state(State.ERROR)
-    #     else:
-    #         self.state = self.switch_state(State.END)
-    #     finally:
-    #         self.end()
+    def run_to_counter_value(self, direction: str, counter: str, trigger_value: int, timeout_in_s=10, as_thread=False):
+        '''Runs the Conveyor until the trigger_value of encoder is reached.
+        
+        :direction: Actuator direction, (last part of whole name)
+        :counter: Counter sensor that is checked with trigger_value
+        :trigger_value: Value at which to stop Conveyor
+        :timeout_in_s: Time after which an exception is raised
+        :as_thread: Runs the function as a thread
+        '''
+        # call this function again as a thread
+        if as_thread == True:
+            self.thread = threading.Thread(target=self.run_to_counter_value, args=(direction, counter, trigger_value, timeout_in_s), name=self.name)
+            self.thread.start()
+            return
+
+        self.state = self.switch_state(State.RUN)
+        try:
+            encoder = Sensor(self.revpi, counter)
+            encoder.reset_encoder()
+            Actuator(self.revpi, self.name).run_to_encoder_value(direction, encoder, trigger_value, timeout_in_s)
+        except Exception as error:
+            log.exception(error)
+            self.state = self.switch_state(State.ERROR)
+            self.error_exception_in_machine = True
+        else:
+            self.state = self.switch_state(State.END)
+            self.ready_for_transport = True
         
