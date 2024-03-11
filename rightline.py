667be305fb255bc21a67fbc8d26d83f9b6eb458a
LUGGGI(Lukas Beck) <st166506@stud.uni-stuttgart.de>
2024-03-11 16:14:30 +0100

    vg1_param_v2

--- rightline.py
+++ rightline.py
@@ -404,27 +404,27 @@ class RightLine(MainLine):
     def run_vg1(self) -> False:
         vg: VacRobot = self.get_machine("VG1", VacRobot, Position(-1, -1, 0))
 
-        # init for retrieve
+         # init
+        if vg.is_position(0):
+            vg.init()
+
+        # move to wh
         if self.state == State.WH_RETRIEVE or self.state == State.VG1_RETRIEVE:
-            if vg.is_position(0):
-                vg.init()
             if vg.is_position(1):
-                # move to wh if new vg1
                 vg.move_to_position(Position(1800, 100, 0), ignore_moving_pos=True)
-        # init for store
+        # move to cb4_start
         else:
             if self.state_is_free(State.WH):
+                # init wh
                 self.run_wh()
-            if vg.is_position(0):
-                vg.init()
             if vg.is_position(1):
                 # move to cb4_start
-                vg.move_to_position(Position(0, 1350, 1150), ignore_moving_pos=True)
+                vg.move_to_position(Position(0, 1375, 1150), ignore_moving_pos=True)
 
         if self.state == State.VG1_STORE:
             if vg.is_position(2):
                 # move down, grip product, move up
-                vg.get_product(1400, sensor="CB4_SENS_START")
+                vg.get_product(1450, sensor="CB4_SENS_START")
             elif vg.is_position(3):
                 self.product_at = vg.name
                 # move to wh
@@ -445,7 +445,7 @@ class RightLine(MainLine):
             elif vg.is_position(3):
                 self.product_at = vg.name
                 # move to cb4_start
-                vg.move_to_position(Position(0, 1350, 1100), ignore_moving_pos=True)
+                vg.move_to_position(Position(0, 1375, 1100), ignore_moving_pos=True)
 
             elif vg.is_position(4) and State.CB4_TO_CB5.value[1] == Status.FREE:
                 # move down
